688e960e89b9f3d4f52dba06f0df2ca6480cd376
Deepti-1010 <deepumalkani1010@gmail.com>
2025-08-23 17:50:35 +0530

    changes

--- src/pages/selleronboardingform.jsx
+++ src/pages/selleronboardingform.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function SellerOnboardingForm() {
+function selleronboardingform() {
   const [formData, setFormData] = useState({
     sellerName: "",
     phone: "",
