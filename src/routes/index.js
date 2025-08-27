b27d85fb0d6c598fc5b974ed675469bc3fd29caa
Deepti-1010 <deepumalkani1010@gmail.com>
2025-08-27 22:04:36 +0530

    L

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 import { lazy } from "react";
-import { GridView, Inventory2Outlined, SupervisorAccountOutlined,ShoppingCart,Storefront} from "@mui/icons-material";
+import { GridView, Inventory2Outlined, SupervisorAccountOutlined, ShoppingCart, Storefront } from "@mui/icons-material";
 const ProductOnboarding = lazy(() => import("../pages/ProductOnboarding"));
 const CategoriesPage = lazy(() => import("../pages/CategoriesPage"));
 const Dashboard = lazy(() => import("../pages/Dashboard"));
 const Products = lazy(() => import("../pages/Products"));
 const Customers = lazy(() => import("../pages/Customers"));
 const Orders = lazy(() => import("../pages/Orders"));
-const Inventory= lazy(() => import("../pages/Inventory"));
+const Inventory = lazy(() => import("../pages/Inventory"));
 const ProductDetails = lazy(() => import("../pages/ProductDetails"));
 const selleronboardingform = lazy(() => import("../pages/selleronboardingform"));
 const operationsonboardingform = lazy(() => import("../pages/operationsonboardingform"));
@@ -15,44 +15,44 @@ const routes = [
   {
     path: "/dashboard",
     component: Dashboard,
-    access : ["admin","seller"]
+    access: ["admin", "seller"]
   },
   {
-  path: "/product-details/:id",
-  component: ProductDetails,
-  access: ["operations","seller"]
-},
+    path: "/product-details/:id",
+    component: ProductDetails,
+    access: ["operations", "seller"]
+  },
   {
     path: "/inventory",
-    component : Inventory,
-    access : ["admin","seller"]
+    component: Inventory,
+    access: ["admin", "seller"]
   },
   {
     path: "/products",
     component: Products,
-    access : ["admin","seller"]
+    access: ["admin", "seller"]
   },
   {
     path: "/customers",
     component: Customers,
-    access : ["admin","seller"]
+    access: ["admin", "seller"]
   },
   {
     path: "/orders",
     component: Orders,
-    access : ["admin","seller"]
+    access: ["admin", "seller"]
   },
   {
     path: "/product-onboarding",
-    component : ProductOnboarding,
-    access : ["operations"]
+    component: ProductOnboarding,
+    access: ["operations"]
   },
-   {
+  {
     path: "/categories",
     component: CategoriesPage,
     access: ["operations"]
   },
-   {
+  {
     path: "/selleronboardingform",
     component: selleronboardingform,
     access: ["seller"]
@@ -68,52 +68,57 @@ const routes = [
 const sidebarRoutes = [
   {
     path: "/dashboard",
-    icon: GridView, 
-    name: "Dashboard", 
-    access : ["admin","seller"]
+    icon: GridView,
+    name: "Dashboard",
+    access: ["admin", "seller"]
   },
   {
     icon: Inventory2Outlined,
     name: "Products",
     path: "/products",
-    access : ["admin","seller"]
+    access: ["admin", "seller"]
   },
   {
-    path : "/inventory",
-    icon: Storefront ,
-    name : "Inventory",
-    access : ["admin","seller"]
+    path: "/inventory",
+    icon: Storefront,
+    name: "Inventory",
+    access: ["admin", "seller"]
   },
   {
     path: "/customers",
     icon: SupervisorAccountOutlined,
     name: "Customers",
-    access : ["admin","seller"]
+    access: ["admin", "seller"]
   },
   {
     path: "/orders",
     icon: ShoppingCart,
     name: "Orders",
-    access : ["admin","seller"]
+    access: ["admin", "seller"]
   },
   {
-    path : "/product-onboarding",
+    path: "/product-onboarding",
     icon: ShoppingCart,
-    name : "Onboarding",
-    access : ["operations"]
+    name: "Onboarding",
+    access: ["operations"]
   },
- {
-  path: "/categories",
-  icon: ShoppingCart,
-  name : "Category",
-  access: ["operations"]  
-},
- {
-  path: "/selleronboardingform",
-  icon: ShoppingCart,
-  name : "selleronboardingform",
-  access: ["seller"]  
-},
+  {
+    path: "/categories",
+    icon: ShoppingCart,
+    name: "Category",
+    access: ["operations"]
+  },
+  {
+    path: "/selleronboardingform",
+    icon: ShoppingCart,
+    name: "selleronboardingform",
+    access: ["seller"]
+  },
+  {
+    path: "/operationsonboardingform",
+    component: operationsonboardingform,
+    access: ["operations"]
+  },
 ];
 
 
